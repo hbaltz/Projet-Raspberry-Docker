83017ebad8c4c41a77964e2f7674141f7ec957d8
hbaltz <hugo.baltz@ensg.eu>
2017-02-20 17:31:57 +0100

    MAJ ftp

--- Stack/prod/start_stacks.sh
+++ Stack/prod/start_stacks.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Deployer le suvi d'état des raspberry pi
 docker stack deploy -c docker-config-influxdb.yml metrics
+
+# Deployer rok4
 docker stack deploy -c docker-config-rok4.yml rok4
+
+# Deployer ftp
 docker stack deploy -c docker-config-ftp.yml ftp
